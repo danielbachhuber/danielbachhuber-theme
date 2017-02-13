64a4c2ec295504fb0f9e7cbf7c13afd57da64d72
Daniel Bachhuber <daniel@handbuilt.co>
2017-02-13 10:52:47 -0800

    Add a basic 404 page

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+	<div class="site-content">
+
+		<div class="row">
+			<div class="columns medium-8 medium-centered">
+				<header class="entry-header">
+					<h1>Page Not Found</h1>
+				</header>
+
+				<div class="entry-content">
+					<p>What you're looking for isn't here... unless you're looking for the 404 page.</p>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+<?php get_footer(); ?>
