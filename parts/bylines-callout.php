e8d184268dc7271759eec2afb018d35070e9e5f4
Daniel Bachhuber <daniel@handbuilt.co>
2017-05-16 15:34:14 -0700

    Less obnox

--- parts/bylines-callout.php
+++ parts/bylines-callout.php
@@ -1,5 +1,3 @@
 <div class="panel callout">
-	<p>Hi!</p>
-	<p>If you like my WordPress work, check out my new plugin, <a href="https://bylines.io/?utm_source=dbcom&utm_medium=web&utm_campaign=begin">Bylines</a>.</p>
-	<p>Thanks!</p>
+	<p>If you like my WordPress work, check out my new plugin, <a href="https://bylines.io/?utm_source=dbcom&utm_medium=web&utm_campaign=begin">Bylines</a>. Thanks!</p>
 </div>
