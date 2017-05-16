861d64f2fe7f1ad7a276f4d3e382e66ca646eef0
Daniel Bachhuber <daniel@handbuilt.co>
2017-05-16 13:44:35 -0700

    More prominent and concise

--- parts/bylines-callout.php
+++ parts/bylines-callout.php
@@ -1,5 +1,5 @@
 <div class="panel callout">
 	<p>Hi!</p>
-	<p>I'm working on a new plugin called <a href="https://bylines.io/?utm_source=dbcom&utm_medium=web&utm_campaign=begin">Bylines</a>. If you like my WordPress work, you should check it out and <a href="https://bylines.io/newsletter/?utm_source=dbcom&utm_medium=web&utm_campaign=begin">sign up for updates</a>.</p>
+	<p>If you like my WordPress work, check out my new plugin, <a href="https://bylines.io/?utm_source=dbcom&utm_medium=web&utm_campaign=begin">Bylines</a>.</p>
 	<p>Thanks!</p>
 </div>
